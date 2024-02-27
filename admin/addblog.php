fea230af79dadca898789bc8d1b33a4bed39c022
MuhammadTahaburiro <159900204+MuhammadTahaburiro@users.noreply.github.com>
2024-02-27 11:52:06 +0500

    update admin pages

--- admin/addblog.php
+++ admin/addblog.php
@@ -5,7 +5,7 @@
 <div class="container-xxl py-6">
         <div class="container">
             <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <h1 class="display-5 mb-3">Add Products</h1>
+                <h1 class="display-5 mb-3">Add Blog</h1>
                 <!-- <p>Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p> -->
             </div>
             <div class="row g-5 justify-content-center">
@@ -16,19 +16,19 @@
                         <div class="row g-3">
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input name="product_name" type="text" class="form-control" id="product_name" placeholder="Product Name">
-                                    <label for="product_name">Product Name</label>
+                                    <input name="Blog_title" type="text" class="form-control" id="Blog_title" placeholder="Blog title">
+                                    <label for="Blog_title">Blog Title</label>
                                 </div>
                             </div>
                             
                             <div class="col-12">
                                 <div class="form-floating">
                                     <textarea class="form-control" placeholder="Add product description here" id="description" style="height: 200px"></textarea>
-                                    <label for="description">Product Description</label>
+                                    <label for="description">Blog Description</label>
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary rounded-pill py-3 px-5" type="submit">Upload Product</button>
+                                <button class="btn btn-primary rounded-pill py-3 px-5" type="submit">Upload Blog</button>
                             </div>
                         </div>
                     </form>
