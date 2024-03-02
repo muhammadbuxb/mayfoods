318ad3167fff86283c478fde45a56fe869f79b79
MuhammadTahaburiro <159900204+MuhammadTahaburiro@users.noreply.github.com>
2024-03-02 14:58:58 +0500

    testinomial page main heading update

--- testimonial.php
+++ testimonial.php
@@ -4,12 +4,12 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s">
         <div class="container">
-            <h1 class="display-3 mb-3 animated slideInDown">Testimonial</h1>
+            <h1 class="display-3 mb-3 animated slideInDown">Reviews</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb mb-0">
                     <li class="breadcrumb-item"><a class="text-body" href="index.php">Home</a></li>
                     <!-- <li class="breadcrumb-item"><a class="text-body" href="#">Pages</a></li> -->
-                    <li class="breadcrumb-item text-dark active" aria-current="page">Testimonial</li>
+                    <li class="breadcrumb-item text-dark active" aria-current="page">Reviews</li>
                 </ol>
             </nav>
         </div>
